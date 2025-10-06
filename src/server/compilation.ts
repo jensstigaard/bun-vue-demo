3aab315e5e0000cdbc52ff6f59d58db6e0350b56
Jens Grønhøj Stigaard <jst046@politi.dk>
2025-10-06 02:53:11 +0200

    feat: add path resolution for serving maps.pmtiles file

--- src/server/compilation.ts
+++ src/server/compilation.ts
@@ -3,6 +3,7 @@
 // 1. Compile the project using bun run build
 // 2. Once built, run : bunx make-vfs --dir ./dist --content-format string --outfile ./src/client-bundle-vfs.js
 
+import { resolve } from "path"
 // First we need to
 // @ts-ignore
 import staticRoutes from "../../bundle/client-bundle-vfs.ts"
@@ -18,8 +19,16 @@ const app = express()
 // Use the routes from the routes file
 app.use(routes)
 
-// Serve static files from this directory
-app.use(express.static("."))
+// // Serve static files from this directory
+// app.use(express.static("."))
+
+app.get("/maps.pmtiles", (req, res) => {
+  const filePath = resolve(process.cwd(), "./maps.pmtiles")
+  // const filePath = resolve(__dirname, "../maps.pmtiles")
+  // console.log("Serving file:", filePath)
+  res.sendFile(filePath)
+  return
+})
 
 // Serve the static files from the staticRoutes
 app.use((req, res, next) => {

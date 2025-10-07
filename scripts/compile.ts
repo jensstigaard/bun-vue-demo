eb20ed4705fd5f62e2c846926cc146bae12591a9
Jens Grønhøj Stigaard <jst046@politi.dk>
2025-10-07 09:20:06 +0200

    fix: add comments to clarify build steps in compile.ts

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -1,9 +1,12 @@
 import { $ } from "bun"
 
+// Build front-end related code with Vite
 await $`vite build`
 
+// Create a virtual file system (VFS) for the client bundle
 await $`bun ./scripts/make-vfs-ck.ts --dir ./dist --outfile ./bundle/client-bundle-vfs.ts`
 
+// Build the server bundle with Bun and output single file executable, including the injected VFS
 await $`bun build ./src/server/compilation.ts --compile --outfile my-app`
 
 console.log("Build complete.")
